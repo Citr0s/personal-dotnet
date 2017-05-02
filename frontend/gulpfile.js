03b6e3c3e33be0da0be93c5a6d145bdd301a8fc1
Milosz Dura <milosz.md@wp.pl>
2017-05-02 21:35:44 +0100

    adding basic boilerplate frontned

---
+++ frontend/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-csso');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+
+gulp.task('css', function(){
+  return gulp.src('sass/*.scss')
+    .pipe(sass())
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', function(){
+  return gulp.src([
+  			'js/jquery.js',
+  			'js/bootstrap.js',
+  			'js/moment.js',
+  			'js/app.js'
+		])
+  	.pipe(concat('app.js'))
+  	.pipe(uglify())
+  	.pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['css', 'js']);
+gulp.watch('sass/*.scss', ['css']);
+gulp.watch('js/*.js', ['js']);
